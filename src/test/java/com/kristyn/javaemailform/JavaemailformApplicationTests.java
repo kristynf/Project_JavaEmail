390d131928faab4f1fbb5d4d1715c776fb42515c
kristynf <kristynf@bu.edu>
2020-03-07 10:24:44 -0500

    initial commit

---
+++ src/test/java/com/kristyn/javaemailform/JavaemailformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.javaemailform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaemailformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
